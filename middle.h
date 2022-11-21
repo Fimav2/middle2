5c8eb36757b25c0499721fa77b8a140eeabddd28
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-21 16:02:07 +0300

    Add files via upload

---
+++ middle.h
@@ -0,0 +1,28 @@
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+
+long long my_abs(long long number);
+
+void itc_num_print(int number);
+
+int itc_len_num(long long number);
+
+int itc_sum_num(long long number);
+
+long long itc_multi_num(long long number);
+
+int itc_max_num(long long number);
+
+int itc_min_num(long long number);
+
+int itc_rev_num(long long number);
+
+int itc_null_count(long long number);
+
+bool itc_mirror_num(long long number);
+
+
+
+
+
+#endif
