a1661406b2cb4b0acb7ddf01869a2bb1cc3aee31
Fimav2 <113976105+Fimav2@users.noreply.github.com>
2022-11-22 12:43:32 +0300

    Update middle.cpp

--- middle.cpp
+++ middle.cpp
@@ -12,89 +12,61 @@ long long my_abs(long long number){
     }
 }
 
+void itc_num_print(int number){
+    cout<< number;
+}
 
-int itc_min_num(long long number){
+
+int itc_len_num(long long number){
 number = my_abs(number);
-    int zifra,minimum;
-    minimum = 9;
-
-    while (number >0){
-        zifra = number % 10;
-
-        if(zifra<minimum){
-            minimum = zifra;
-        }
-
-        number /= 10;
-    }
-
-    return minimum;
-
+    int rezult;
+    rezult=0;
+    while (number>0){
+        rezult = rezult +1;
+        number = number/10;
 }
 
-int itc_rev_num(long long number){
-
-    return -1;
-
+    return rezult;
 }
 
-int itc_null_count(long long number){
+int itc_sum_num(long long number){
     number = my_abs(number);
-    int zifra, nullCount;
-    nullCount = 0;
-    while(number > 0){
-        zifra = number % 10;
+int symma,zifra;
+symma = 0 ;
+while (number>0){
+        zifra = number %10;
+        symma = zifra + symma;
+        number = number/10;
+}
+return symma;
 
-        if (zifra == 0){
-            nullCount = nullCount + 1;
-        }
 
-        number /= 10;
-    }
 
-    return nullCount;
 
 }
+long long itc_multi_num(long long number){
+    number = my_abs(number);
+int zifra,proiz;
+    proiz = 1;
+while (number>0){
+    zifra = number %10;
+    proiz = proiz * zifra ;
+    number = number /10;
+}
+return proiz;
+}
 
-bool itc_mirror_num(long long number){
-int zifra,x,y,b;
-    y = 0;
-    b = number;
-    if (b / 10 != 0 )
-        {
-            if (b > 0)
-                {
-                    while (b > 0)
-                    {
-                        zifra = b % 10;
-                        y =  y*10  + zifra;
-                        b = b / 10;
-                    }
 
-            if (number == y)
-                return 1;
-                    return 0;
-                }
-                else
-                {
-                    b = b * (-1);
-                    while (b > 0)
-                    {
-                        zifra = b % 10;
-                        y =  y*10  + zifra;
-                        b = b / 10;
-                    }
-
-            if (number == y * (-1))
-                return 1;
-                    return 0;
-                }
+int itc_max_num(long long number){
+    number = my_abs(number);
+    int zifra, great;
+    great = 0;
+    while(number>0){
+        zifra = number % 10;
+        if (zifra > great){
+            great = zifra;
         }
-    return -1;
-
-
-
-
-
-
+        number /=  10;
+    }
+    return great;
 }
